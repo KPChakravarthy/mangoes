c50335bd4c429f32640d29038e110862cec2a10a
krishna <chakravarthy1165@gmail.com>
2026-05-31 13:13:23 +0530

    Updated base path

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: './',
+  base: '/mangoes/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
